d9a89ece2725b88d04aae3732f3fcc28873a6c11
Eric Quanz <2374654+ericorruption@users.noreply.github.com>
2022-03-25 10:40:29 +0100

    Temperature converter WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import type { FunctionComponent } from 'react';
+
 import './App.css';
 
-function App() {
+import { Counter } from './counter/Counter';
+import { TemperatureConverter } from './temperature-converter/TemperatureConverter';
+
+type Challenges = 'counter';
+
+const App: FunctionComponent = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <Counter /> */}
+      <TemperatureConverter />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/temperature-converter/TemperatureConverter.tsx
@@ -0,0 +1,55 @@
+import { ChangeEvent, FunctionComponent, useState } from 'react';
+
+export const TemperatureConverter: FunctionComponent = () => {
+  const [celsius, setCelsius] = useState<number>();
+  const [fahrenheit, setFahrenheit] = useState<number>();
+
+  const handleCelsiusChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = parseFloat(event.target.value);
+
+    if (isNaN(value)) {
+      setCelsius(undefined);
+      return;
+    }
+
+    setCelsius(value);
+    setFahrenheit(value * (9 / 5) + 32);
+  };
+
+  const handleFahrenheitChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = parseFloat(event.target.value);
+
+    if (isNaN(value)) {
+      setFahrenheit(undefined);
+      return;
+    }
+
+    setFahrenheit(value);
+    setCelsius((value - 32) * (5 / 9));
+  };
+
+  return (
+    <>
+      <label>
+        <input
+          type="number"
+          onChange={handleCelsiusChange}
+          value={celsius?.toFixed(1) ?? ''}
+        />{' '}
+        Celsius
+      </label>{' '}
+      ={' '}
+      <label>
+        <input
+          type="number"
+          onChange={handleFahrenheitChange}
+          value={fahrenheit?.toFixed(1) ?? ''}
+        />{' '}
+        Fahrenheit
+      </label>
+    </>
+  );
+};
+
+// TODO
+// improve input handling (especially of period, comma)
